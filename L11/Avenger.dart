1212acc476105dce20bed6bea889d7df1d1ca90b
Light <light.et@gmail.com>
2019-06-19 18:07:17 +0700

    commit L11

--- L11/Avenger.dart
+++ L11/Avenger.dart
@@ -1,38 +1,83 @@
-import 'CaptainAmerica.dart';
-import 'Thanos.dart';
-import 'Thor.dart';
-
-abstract class Avenger {
-  String name;
-  String sexual;
-  Avenger.forExtendConstructor({String name = "Avenger", String sexual = "Unkown"}) {
-    this.name = name;
-    this.sexual = sexual;
-  }
-  void showInfo() {
-    print('------ Info ------');
-    print('Name: $name');
-    print('Sexual: $sexual');
-  }
-
-  factory Avenger({String type, String name = "Avenger", String sexual = "Unkown"}){
-    dynamic avenger;
-    switch(type){
-      case Avengers.Thanos:  avenger = Thanos(name:name, sexual: sexual); break;
-      case Avengers.Thor:  avenger = Thor(name:name, sexual: sexual); break;
-      case Avengers.CaptainAmerica:  avenger = CaptainAmerica(name:name, sexual: sexual); break;
-    }
-    return avenger;
-  }
-  // add more named constructor function here
-
-
-  void doSkill();
-}
-class Avengers {
-  static const String Thanos = 'Thanos';
-  static const String CaptainAmerica = 'Captain America';
-  static const String Thor = 'Thor';
-  // add more function here
-  
-}
+import 'dart:convert' as JSON; 
+import 'package:http/http.dart' as http; 
+import 'CaptainAmerica.dart'; 
+import 'Thanos.dart'; 
+import 'Thor.dart'; 
+ 
+Future<dynamic> fetchAvenger(url) async { 
+  try { 
+    final response = await http.get(url); 
+    if (response.statusCode == 200) { 
+      //print(JSON.jsonDecode((response.body))); 
+      return JSON.jsonDecode((response.body)); 
+    } else { 
+      print('Failed to load post'); 
+    } 
+  } catch (e) { 
+    print(e); 
+  } 
+} 
+ 
+ 
+ 
+abstract class Avenger { 
+  String name; 
+  String sexual; 
+  String url; 
+  Avenger.forExtendConstructor( 
+      {String name = "Avenger", String sexual = "Unkown"}) { 
+    this.name = name; 
+    this.sexual = sexual; 
+  } 
+  void showInfo() { 
+    print('------ Info ------'); 
+    print('Name: $name'); 
+    print('Sexual: $sexual'); 
+  } 
+ 
+  factory Avenger( 
+      {String type, String name = "Avenger", String sexual = "Unkown"}) { 
+    dynamic avenger; 
+    switch (type) { 
+      case Avengers.Thanos: 
+        avenger = Thanos(name: name, sexual: sexual); 
+        break; 
+      case Avengers.Thor: 
+        avenger = Thor(name: name, sexual: sexual); 
+        break; 
+      case Avengers.CaptainAmerica: 
+        avenger = CaptainAmerica(name: name, sexual: sexual); 
+        break; 
+    } 
+    return avenger; 
+  } 
+   
+  factory Avenger.fromURL( 
+      {String type, String name = "Avenger", String sexual = "Unkown"}) { 
+    dynamic avenger; 
+    switch (type) { 
+      case Avengers.Thor: 
+        avenger = CaptainAmerica(name: name, sexual: sexual); 
+        break; 
+    } 
+    return avenger; 
+  } 
+  void doSkill(); 
+} 
+class Avengers { 
+  static dynamic getFileNameFromUrl(String url) { 
+    var filename = url.split("/").last; 
+    var type = filename.split(".").first; 
+    return type; 
+  } 
+  static const String Thanos = 'Thanos'; 
+  static const String CaptainAmerica = 'CaptainAmerica'; 
+  static const String Thor = 'Thor'; 
+ 
+  static dynamic getJsonUrl(url) async { 
+    var name = await fetchAvenger(url); 
+    var getJson = name["Avenger"]; 
+    return  getJson; 
+  } 
+} 
+ 
--- L11/L11.dart
+++ L11/L11.dart
@@ -1,19 +1,37 @@
-// import file here
-
-const List<String> urlAvengers = [
-  'https://blogspotscraping.herokuapp.com/avengers/Thor.json',
-  'https://blogspotscraping.herokuapp.com/avengers/Thanos.json',
-  'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json',
-];
-
-//funtion createAvengers(){
-  // todo
-  // forced call default factory constructor
-//}
-
-void main(){
-  print('============ L11.2 - Call Default Factory Constructor Orderly ============');
-  // todo
-  print('============ L11.1 - Call Named factory constructor create any avenger ============');
-  // todo
-}
+import 'Thanos.dart'; 
+import 'Avenger.dart'; 
+import 'CaptainAmerica.dart'; 
+import 'Thor.dart'; 
+ 
+const List<String> urlAvengers = [ 
+  'https://blogspotscraping.herokuapp.com/avengers/Thor.json', 
+  'https://blogspotscraping.herokuapp.com/avengers/Thanos.json', 
+  'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json', 
+]; 
+ 
+dynamic createAvengers() async { 
+  for (int i = 0; i < urlAvengers.length; i++) { 
+    var val = await Avengers.getJsonUrl(urlAvengers[i]); 
+    var filename = urlAvengers[i].split("/").last; 
+    var type = filename.split(".").first; 
+    Avenger avengers = 
+        Avenger(type: type, name: val["name"], sexual: val["sexual"]); 
+    avengers.showInfo(); 
+  } 
+} 
+ 
+void main() async { 
+  print( 
+      '============ L11.1 - Call Named factory constructor create any avenger ============'); 
+  var val = await Avengers.getJsonUrl( 
+      'https://blogspotscraping.herokuapp.com/avengers/Thor.json'); 
+  Thor thor = Avenger.fromURL( 
+      type: Avengers.getFileNameFromUrl( 
+          'https://blogspotscraping.herokuapp.com/avengers/Thor.json'), 
+      name: val["name"], 
+      sexual: val["sexual"]); 
+  thor.showInfo(); 
+    print( 
+      '============ L11.2 - Call Default Factory Constructor Orderly ============'); 
+  createAvengers(); 
+} 
