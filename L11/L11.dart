e965c7cfa45fa0aa1959f5b8fc24478203a1b9b2
Light <light.et@gmail.com>
2019-06-21 18:04:49 +0700

    L11

--- L11/L11.dart
+++ L11/L11.dart
@@ -1,37 +1,52 @@
-import 'Thanos.dart'; 
-import 'Avenger.dart'; 
-import 'CaptainAmerica.dart'; 
-import 'Thor.dart'; 
- 
-const List<String> urlAvengers = [ 
-  'https://blogspotscraping.herokuapp.com/avengers/Thor.json', 
-  'https://blogspotscraping.herokuapp.com/avengers/Thanos.json', 
-  'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json', 
-]; 
- 
-dynamic createAvengers() async { 
-  for (int i = 0; i < urlAvengers.length; i++) { 
-    var val = await Avengers.getJsonUrl(urlAvengers[i]); 
-    var filename = urlAvengers[i].split("/").last; 
-    var type = filename.split(".").first; 
-    Avenger avengers = 
-        Avenger(type: type, name: val["name"], sexual: val["sexual"]); 
-    avengers.showInfo(); 
-  } 
-} 
- 
-void main() async { 
-  print( 
-      '============ L11.1 - Call Named factory constructor create any avenger ============'); 
-  var val = await Avengers.getJsonUrl( 
-      'https://blogspotscraping.herokuapp.com/avengers/Thor.json'); 
-  Thor thor = Avenger.fromURL( 
-      type: Avengers.getFileNameFromUrl( 
-          'https://blogspotscraping.herokuapp.com/avengers/Thor.json'), 
-      name: val["name"], 
-      sexual: val["sexual"]); 
-  thor.showInfo(); 
-    print( 
-      '============ L11.2 - Call Default Factory Constructor Orderly ============'); 
-  createAvengers(); 
-} 
+import '../L8/L8.dart';
+import 'Thanos.dart';
+import 'Avenger.dart';
+import 'CaptainAmerica.dart';
+import 'Thor.dart';
+
+const List<String> urlAvengers = [
+  'https://blogspotscraping.herokuapp.com/avengers/Thor.json',
+  'https://blogspotscraping.herokuapp.com/avengers/Thanos.json',
+  'https://blogspotscraping.herokuapp.com/avengers/CaptainAmerica.json',
+];
+
+// dynamic createAvengers() async {
+//   for (int i = 0; i < urlAvengers.length; i++) {
+//     var val = await Avengers.getJsonUrl(urlAvengers[i]);
+//     var filename = urlAvengers[i].split("/").last;
+//     var type = filename.split(".").first;
+//     Avenger avengers =
+//         Avenger(type: type, name: val["name"], sexual: val["sexual"]);
+//     avengers.showInfo();
+//   }
+// }
+createAvengers() async {
+  for (final i in urlAvengers) {
+    Future<String> aa =await Avengers.getFileNameFromUrl(i);
+    aa.then((onValue) {
+      print(onValue);
+    });
+    // var val = await Avengers.getJsonUrl(i);
+    // var filename = i.split("/").last;
+    // var type = filename.split(".").first;
+    // Avenger avengers =
+    //     Avenger(type: type, name: val["name"], sexual: val["sexual"]);
+    // avengers.showInfo();
+  }
+}
+
+void main() async {
+  print(
+      '============ L11.1 - Call Named factory constructor create any avenger ============');
+  var val = await Avengers.getJsonUrl(
+      'https://blogspotscraping.herokuapp.com/avengers/Thor.json');
+  Thor thor = Avenger.fromURL(
+      type: Avengers.getFileNameFromUrl(
+          'https://blogspotscraping.herokuapp.com/avengers/Thor.json'),
+      name: val["name"],
+      sexual: val["sexual"]);
+  thor.showInfo();
+  print(
+      '============ L11.2 - Call Default Factory Constructor Orderly ============');
+  createAvengers();
+}
